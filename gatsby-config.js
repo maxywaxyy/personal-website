97a0edfc312c81699551b03e62badb2f37cb6684
Max <maxlee10@g.ucla.edu>
2024-01-08 17:16:43 -0800

    installed gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,26 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `pages`,
+        // Path to the directory
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        // Ignore files starting with a dot
+        ignore: [`**/\.*`],
+        // Use "mtime" and "inode" to fingerprint files (to check if file has changed)
+        fastHash: true,
+      },
+    },
     `gatsby-plugin-material-ui`,
     `gatsby-plugin-image`,
     {
